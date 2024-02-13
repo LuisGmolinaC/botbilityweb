2b36e7227bf2d0c8dec12bf49e387c370e9b083d
Luis Molina <luistarstoned@gmail.com>
2024-02-13 16:06:27 -0500

    commitngweb

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './components/about/about.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { NavComponent } from './components/nav/nav.component';
+import { ServicesComponent } from './components/services/services.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { PricingComponent } from './components/pricing/pricing.component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, AboutComponent, ClientsComponent, GalleryComponent, NavComponent, ServicesComponent, TestimonialsComponent, PricingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
